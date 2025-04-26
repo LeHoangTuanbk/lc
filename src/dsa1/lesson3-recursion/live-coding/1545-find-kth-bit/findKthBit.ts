1d95a4600e199f9e419c4cfa9ea44f90db8d593e
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-04-26 19:45:06 +0900

    Add: FindKbith

--- src/dsa1/lesson3-recursion/live-coding/1545-find-kth-bit/findKthBit.ts
+++ src/dsa1/lesson3-recursion/live-coding/1545-find-kth-bit/findKthBit.ts
@@ -50,28 +50,16 @@ function findKthBit3(n: number, k: number): string {
 }
 // Time: O(n), Space: O(n)
 function findKthBit(n: number, k: number): string {
-  function reverse(s: string): string {
-    return s.split('').reverse().join('');
+  function compute(n: number, k: number, inverse: boolean): string {
+    if (n == 1) return inverse ? '1' : '0';
+    let lenSn = Math.pow(2, n) - 1;
+    const mid = Math.floor(lenSn / 2) + 1;
+    if (mid == k) return inverse ? '0' : '1';
+    if (mid > k) return compute(n - 1, k, inverse);
+    k -= mid;
+    k = mid - k;
+    return compute(n - 1, k, !inverse);
   }
 
-  function invert(s: string) {
-    return s
-      .split('')
-      .map((c) => {
-        if (c === '0') return '1';
-        return '0';
-      })
-      .join('');
-  }
-  function calculateSn(n: number): string {
-    let snArray: string[] = new Array(n + 1);
-    snArray[1] = '0';
-    snArray[2] = '011';
-    for (let i = 3; i <= n; i++) {
-      snArray[i] = snArray[i - 1] + '1' + reverse(invert(snArray[i - 1]));
-    }
-    return snArray[n];
-  }
-  const s = calculateSn(n);
-  return s[k - 1];
+  return compute(n, k, false);
 }
