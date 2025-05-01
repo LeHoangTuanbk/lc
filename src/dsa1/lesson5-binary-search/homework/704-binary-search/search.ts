2ebd7a2017c82ef3b5bdb302356c22747605f856
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-05-01 10:03:45 +0900

    Refactor: binary search

---
+++ src/dsa1/lesson5-binary-search/homework/704-binary-search/search.ts
@@ -0,0 +1,82 @@
+export function search2(nums: number[], target: number): number {
+  let low = 0,
+    high = nums.length - 1,
+    mid;
+
+  while (low <= high) {
+    mid = low + Math.floor((high - low) / 2);
+    if (nums[mid] == target) {
+      return mid;
+    }
+    if (nums[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
+}
+
+export function search(nums: number[], target: number): number {
+  function recursiveBinarySearch(nums: number[], target: number, low: number, high: number) {
+    if (low > high) return -1;
+    let mid = low + Math.floor((high - low) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] < target) return recursiveBinarySearch(nums, target, mid + 1, high);
+    else return recursiveBinarySearch(nums, target, low, mid - 1);
+  }
+  return recursiveBinarySearch(nums, target, 0, nums.length - 1);
+}
+
+// console.log(search([-1, 0, 3, 5, 9, 12], 2));
+
+export function findLowerUpperBound(nums: number[], target: number): number[] {
+  let low = 0,
+    high = nums.length - 1,
+    mid;
+
+  while (low <= high) {
+    mid = low + Math.floor((high - low) / 2);
+    if (nums[mid] <= target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  if (nums[high] === target) high--;
+  return [high, low];
+}
+
+export function findUpperBound(nums: number[], target: number): number {
+  let low = 0,
+    high = nums.length - 1,
+    mid;
+
+  while (low <= high) {
+    mid = low + Math.floor((high - low) / 2);
+    if (nums[mid] <= target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return low;
+}
+
+export function findPredecessor(nums: number[], target: number): number {
+  let low = 0,
+    high = nums.length - 1,
+    mid;
+
+  while (low <= high) {
+    mid = low + Math.floor((high - low) / 2);
+    if (nums[mid] >= target) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return high;
+}
+
+console.log(findPredecessor([-1, 0, 3, 5, 9, 12], 5));
