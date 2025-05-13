5791ce17821f5db6083ab800849d7e35e83c619c
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-05-13 21:35:13 +0900

    Add: 203

--- src/dsa1/lesson6-linked-list/live-coding/203-remove-linked-list-elements/removeElements.ts
+++ src/dsa1/lesson6-linked-list/live-coding/203-remove-linked-list-elements/removeElements.ts
@@ -1,3 +1,17 @@
 import { ListNode } from '../linkedList';
 
-function removeElements(head: ListNode | null, val: number): ListNode | null {}
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+  let cur = head;
+  while (cur && cur.val === val) {
+    cur = cur.next;
+  }
+  head = cur;
+  while (cur && cur.next) {
+    if (cur.next.val === val) {
+      cur.next = cur.next.next;
+      continue;
+    }
+    cur = cur.next;
+  }
+  return head;
+}
