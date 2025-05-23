3c1eb180ee0c25aadd017f79cc143f7fd00a1399
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-05-23 22:23:43 +0900

    Add: 257

--- src/dsa1/lesson11-graph2/home-work/257-binary-tree-paths/binary-tree-paths.ts
+++ src/dsa1/lesson11-graph2/home-work/257-binary-tree-paths/binary-tree-paths.ts
@@ -1,6 +1,6 @@
 import { TreeNode } from '../../tree-node';
 
-function binaryTreePaths(root: TreeNode | null): string[] {
+function binaryTreePaths2(root: TreeNode | null): string[] {
   const paths: number[][] = [];
   const path: number[] = [];
   function findPaths(node: TreeNode | null) {
@@ -16,4 +16,22 @@ function binaryTreePaths(root: TreeNode | null): string[] {
   findPaths(root);
 
   return paths.map((path) => path.join('->'));
+}
+
+function binaryTreePaths(root: TreeNode | null): string[] {
+  const paths: string[] = [];
+  function dfs(node: TreeNode | null, path: string) {
+    if (node === null) return;
+    path += node.val.toString();
+    if (!node.left && !node.right) {
+      paths.push(path);
+    } else {
+      path += '->';
+      dfs(node.left, path);
+      dfs(node.right, path);
+    }
+  }
+  dfs(root, '');
+
+  return paths;
 }
