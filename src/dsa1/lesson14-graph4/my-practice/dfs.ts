189637c7765535820ef7a999f379045a922c7c99
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-05-27 21:20:40 +0900

    Add: dfs

---
+++ src/dsa1/lesson14-graph4/my-practice/dfs.ts
@@ -0,0 +1,28 @@
+function dfsTraversal(n: number, graph: Map<number, number[]>) {
+  const visited = Array(n).fill(false);
+  const res: number[] = [];
+  function dfs(u: number) {
+    visited[u] = true;
+    res.push(u);
+    for (const v of graph.get(u)!) {
+      if (!visited[v]) {
+        dfs(v);
+      }
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      dfs(i);
+    }
+  }
+  return res;
+}
+
+const n = 4;
+const graph = new Map<number, number[]>();
+graph.set(0, [1, 2]);
+graph.set(1, [0, 3]);
+graph.set(2, [0]);
+graph.set(3, [1]);
+
+console.log(dfsTraversal(n, graph));
