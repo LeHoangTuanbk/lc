f2a57143e17b21507be5145728d49ecc046e98b1
toan rehoan <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-06-30 04:27:09 +0000

    Add: 78 subsets

--- src/dsa2/lesson1/live-coding/78-subsets/subsets.ts
+++ src/dsa2/lesson1/live-coding/78-subsets/subsets.ts
@@ -1,3 +1,18 @@
-export subsets(nums: number[]): number[][] {
-  
-}
+function getBit(x: number, i: number) : number {
+	return (x >> i) & 1;
+};
+
+function subsets(nums: number[]) : number[][] {
+	const n = nums.length;
+	const res : number[][] = [];
+	for(let x = 0; x < (1 << n); x++) {
+		const aSet: number[] = [];
+		for(let i = 0; i < n; i++) {
+			if(getBit(x, i)) {
+				aSet.push(nums[i]);
+			}
+		}
+        res.push(aSet);
+	}	
+	return res;
+};
