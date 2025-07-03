1396c97194b1a4290e7a75c1a12b8f997163e5a7
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-07-03 20:03:22 +0900

    Add: 701

---
+++ src/dsa2/lesson2-advanced-tree/live-coding/homework/701-insert-bst/insert-into-bst.ts
@@ -0,0 +1,35 @@
+import { TreeNode } from '../../../tree-node';
+// Recursive method,Time: O(n), Space: O(h)
+function insertIntoBST2(root: TreeNode | null, val: number): TreeNode | null {
+  if (root === null) return new TreeNode(val);
+  if (root.val < val) root.right = insertIntoBST2(root.right, val);
+  if (root.val > val) root.left = insertIntoBST2(root.left, val);
+  return root;
+}
+
+// Iterative method, Space: O(1)
+function insertIntoBST(root: TreeNode | null, val: number): TreeNode | null {
+  if (root === null) return new TreeNode(val);
+  let cur = root;
+  while (true) {
+    if (cur.val < val) {
+      if (cur.right) {
+        cur = cur.right;
+      } else {
+        cur.right = new TreeNode(val);
+        break;
+      }
+    }
+
+    if (cur.val > val) {
+      if (cur.left) {
+        cur = cur.left;
+      } else {
+        cur.left = new TreeNode(val);
+        break;
+      }
+    }
+  }
+
+  return root;
+}
