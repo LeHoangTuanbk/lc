6b4c3974a1f4d4b09305311054786c7c52b76d46
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-07-24 20:51:41 +0900

    Add: 763

--- src/dsa2/lesson6-2-pointers/homework/763-partition-label/partition-label.ts
+++ src/dsa2/lesson6-2-pointers/homework/763-partition-label/partition-label.ts
@@ -1,8 +1,27 @@
-export function partitionLabels(s: string): number[] {}
+export function partitionLabels(s: string): number[] {
+  const last = Array(26).fill(-1);
+  for (let i = 0; i < s.length; i++) {
+    last[s.charCodeAt(i) - 97] = i;
+  }
+
+  const res: number[] = [];
+  let start = 0,
+    end = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    end = Math.max(end, last[s.charCodeAt(i) - 97]);
+    if (i == end) {
+      res.push(end - start + 1);
+      start = i + 1;
+    }
+  }
+
+  return res;
+}
 /* 
 Example 1:
 
-Input: s = "ababcbacadefegdehijhklij"
+Input: s = "ababcbaca defegde hijhklij"
 Output: [9,7,8]
 Explanation:
 The partition is "ababcbaca", "defegde", "hijhklij".
