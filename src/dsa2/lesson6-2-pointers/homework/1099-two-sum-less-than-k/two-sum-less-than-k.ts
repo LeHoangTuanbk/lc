ad24722a1171d1fa0dd0f5f79a39b7d717e021bc
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-07-24 17:24:59 +0900

    Add: 1099

--- src/dsa2/lesson6-2-pointers/homework/1099-two-sum-less-than-k/two-sum-less-than-k.ts
+++ src/dsa2/lesson6-2-pointers/homework/1099-two-sum-less-than-k/two-sum-less-than-k.ts
@@ -1,4 +1,21 @@
-export function twoSumLessThanK(nums: number[], k: number): number {}
+export function twoSumLessThanK(nums: number[], k: number): number {
+  nums.sort((a, b) => a - b);
+  let left = 0,
+    right = nums.length - 1,
+    sum = 0,
+    closestSum = -Infinity;
+
+  while (left < right) {
+    sum = nums[left] + nums[right];
+    if (sum < k) {
+      closestSum = Math.max(closestSum, sum);
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return closestSum === -Infinity ? -1 : closestSum;
+}
 /* 
 Example 1:
 
@@ -7,6 +24,8 @@ Output: 58
 Explanation: 
 We can use 34 and 24 to sum 58 which is less than 60.
 Example 2:
+
+[10, 20 , 30], k = 25
 
 Input: A = [10,20,30], K = 15
 Output: -1
