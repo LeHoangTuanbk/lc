4fa4c19a8e3339b1a48d3307a16381d7ef0ad54b
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-07-24 10:24:00 +0900

    Add: 15

---
+++ src/dsa2/lesson6-2-pointers/homework/15-3-sum/three-sum.ts
@@ -0,0 +1,31 @@
+export function threeSum(nums: number[]): number[][] {
+  // sort + 2 pointers
+  const res: number[][] = [];
+  const n = nums.length;
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < n - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1;
+    let right = n - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (sum === 0) {
+        res.push([nums[i], nums[left], nums[right]]);
+        // handle duplicated numbers
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return res;
+}
