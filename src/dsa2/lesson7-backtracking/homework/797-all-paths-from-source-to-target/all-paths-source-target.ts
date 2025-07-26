21212b90bade1c747f82c3ab81f0fe1cafd59bdb
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-07-26 12:39:52 +0900

    Add: 797

---
+++ src/dsa2/lesson7-backtracking/homework/797-all-paths-from-source-to-target/all-paths-source-target.ts
@@ -0,0 +1,32 @@
+export function allPathsSourceTarget(graph: number[][]): number[][] {
+  const res: number[][] = [];
+  const n = graph.length;
+  const visited = Array(n).fill(false);
+
+  backtrack(0, [0], visited, res, graph, n);
+
+  return res;
+}
+
+function backtrack(
+  u: number,
+  path: number[],
+  visited: boolean[],
+  res: number[][],
+  graph: number[][],
+  n: number,
+) {
+  if (u === n - 1) {
+    res.push([...path]);
+  }
+
+  for (const v of graph[u]) {
+    if (visited[v]) continue;
+    visited[v] = true;
+    path.push(v);
+    backtrack(v, path, visited, res, graph, n);
+
+    visited[v] = false;
+    path.pop();
+  }
+}
