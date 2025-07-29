e267c157ebd2f5ef9e230d566fefd6166da7adfa
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-07-29 09:44:07 +0900

    Add: unique path

--- src/dsa2/lesson8-backtrack-memoization/live-coding/62-unique-paths/unique-path.ts
+++ src/dsa2/lesson8-backtrack-memoization/live-coding/62-unique-paths/unique-path.ts
@@ -1,5 +1,5 @@
 export function uniquePaths(m: number, n: number): number {
-  const dp = Array({ length: m }, () => Array(n).fill(-1));
+  const dp = Array.from({ length: m }, () => Array(n).fill(-1));
   dp[m - 1][n - 1] = 1;
   return dfs(0, 0, m, n, dp);
 }
@@ -17,7 +17,6 @@ function dfs(x: number, y: number, m: number, n: number, dp: number[][]) {
   if (y < n - 1) {
     val += dfs(x, y + 1, m, n, dp);
   }
-
   dp[x][y] = val;
 
   return val;
