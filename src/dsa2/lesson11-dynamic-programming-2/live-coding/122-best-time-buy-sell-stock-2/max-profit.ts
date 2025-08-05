6503b8b0ba893bb5cb4e608c9f36937ad2a19bf2
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-08-05 15:20:59 +0900

    Add: 122

---
+++ src/dsa2/lesson11-dynamic-programming-2/live-coding/122-best-time-buy-sell-stock-2/max-profit.ts
@@ -0,0 +1,31 @@
+export function maxProfit(prices: number[]): number {
+  const n = prices.length;
+  if (n == 1) return 0;
+  let bestProfit = 0;
+  for (let i = 1; i < n; i++) {
+    if (prices[i] > prices[i - 1]) bestProfit += prices[i] - prices[i - 1];
+  }
+  return bestProfit;
+}
+
+/* 
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Total profit is 4.
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+
+*/
