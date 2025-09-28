0410536916388186eb9d61686d92a3e76fa68994
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-09-28 11:10:56 +0900

    add: 289

--- src/my-practice/matrix/289-game-of-life/solution.ts
+++ src/my-practice/matrix/289-game-of-life/solution.ts
@@ -1,4 +1,41 @@
 /**
  Do not return anything, modify board in-place instead.
  */
-export function gameOfLife(board: number[][]): void {}
+export function gameOfLife(board: number[][]): void {
+  const m = board.length,
+    n = board[0].length;
+  const dirs = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+
+  const copy = board.map((row) => [...row]);
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      let live = 0;
+
+      for (const [dx, dy] of dirs) {
+        const x = i + dx,
+          y = j + dy;
+        if (x >= 0 && x < m && y >= 0 && y < n && copy[x][y] === 1) {
+          live++;
+        }
+      }
+
+      if (copy[i][j] === 1) {
+        if (live < 2 || live > 3) board[i][j] = 0;
+        else board[i][j] = 1;
+      } else {
+        if (live === 3) board[i][j] = 1;
+        else board[i][j] = 0;
+      }
+    }
+  }
+}
